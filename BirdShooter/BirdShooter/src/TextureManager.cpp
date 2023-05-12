b80cdb8ec05c7734337ef9f1c9a21d32a2cd8d56
MTN95 <113786893+MTN95@users.noreply.github.com>
2023-05-12 09:56:45 +0300

    added error handling in TextureManager::RenderText

--- BirdShooter/BirdShooter/src/TextureManager.cpp
+++ BirdShooter/BirdShooter/src/TextureManager.cpp
@@ -9,7 +9,6 @@ namespace mEngine
 
     bool TextureManager::LoadTexture(std::string id, std::string path)
     {
-        //Load image at specified path
         SDL_Surface* loadedSurface = IMG_Load(path.c_str());
         if (loadedSurface == nullptr)
         {
@@ -17,7 +16,6 @@ namespace mEngine
             return false;
         }
 
-        //Create texture from surface pixels
         SDL_Texture* newTexture = SDL_CreateTextureFromSurface(Engine::Getinstance()->GetRenderer(), loadedSurface);
         if (newTexture == nullptr)
         {
@@ -54,7 +52,6 @@ namespace mEngine
 
     void TextureManager::Render(std::string id, float x, float y, float width, float height, float scale, SDL_RendererFlip flip)
     {
-        // Set rendering space and render to screen
         SDL_Rect srcRect = 
         { 
             0, 
@@ -64,12 +61,10 @@ namespace mEngine
         };
         SDL_FRect dstRect = { x, y, width * scale, height * scale};
 
-        // Render to screen
         SDL_RenderCopyExF(Engine::Getinstance()->GetRenderer(), m_TextureMap[id], &srcRect, &dstRect, 0, nullptr, flip);
     }
 	void TextureManager::RenderFrame(std::string id, float x, float y, float width, float height, int row, int frame, float scale, SDL_RendererFlip flip)
 	{
-		// Set rendering space and render to screen
 		SDL_Rect srcRect = 
         { 
             width  * frame,
@@ -79,44 +74,54 @@ namespace mEngine
         };
 		SDL_FRect dstRect = { x, y, width * scale, height * scale };
 
-		// Render to screen
 		SDL_RenderCopyExF(Engine::Getinstance()->GetRenderer(), m_TextureMap[id], &srcRect, &dstRect, 0, nullptr, flip);
 	}
 
-    void TextureManager::RenderText(const std::string& text, int x, int y, TTF_Font* font, SDL_Color color, const int& textScale,unsigned int number)
+	void TextureManager::RenderText(const std::string& text, int x, int y, TTF_Font* font, SDL_Color color, const int& textScale, unsigned int number)
 	{
-        std::string finalText = text;
-        if (number != UINT_MAX)
-        {
-            finalText += " " + std::to_string(number);
-        }
-        
-        SDL_Renderer* renderer = mEngine::Engine::Getinstance()->GetRenderer();
+		std::string finalText = text;
+		if (number != UINT_MAX)
+		{
+			finalText += " " + std::to_string(number);
+		}
+
+		SDL_Renderer* renderer = mEngine::Engine::Getinstance()->GetRenderer();
 		SDL_Surface* surface = CreateTextSurface(finalText, font, color);
+
+		if (surface == nullptr) 
+        {
+            std::cout << "Unable to create Text Surface! SDL Error: " << SDL_GetError();
+			return;
+		}
+
 		SDL_Texture* TextTexture = SDL_CreateTextureFromSurface(renderer, surface);
-        SDL_FreeSurface(surface);
 
-		SDL_Rect rect = { x, y, surface->w * textScale, surface->h * textScale};
+		if (TextTexture == nullptr) {
+            std::cout << "Unable to create Texture From Surface! SDL Error: " << SDL_GetError();
+            SDL_FreeSurface(surface);
+			return;
+		}
 
-        SDL_RenderCopy(renderer, TextTexture, nullptr, &rect);
-        SDL_DestroyTexture(TextTexture);
+		SDL_Rect rect = { x, y, surface->w * textScale, surface->h * textScale };
+
+		SDL_RenderCopy(renderer, TextTexture, nullptr, &rect);
+
+		SDL_DestroyTexture(TextTexture);
+		SDL_FreeSurface(surface);
 	}
 
     void TextureManager::SetColor(std::string id, Uint8 red, Uint8 green, Uint8 blue)
     {
-        //Modulate texture rgb
         SDL_SetTextureColorMod(m_TextureMap[id], red, green, blue);
     }
 
     void TextureManager::SetBlendMode(std::string id, SDL_BlendMode blending)
     {
-        //Set blending function
         SDL_SetTextureBlendMode(m_TextureMap[id], blending);
     }
 
     void TextureManager::SetAlpha(std::string id, Uint8 alpha)
     {
-        //Modulate texture alpha
         SDL_SetTextureAlphaMod(m_TextureMap[id], alpha);
     }
 
